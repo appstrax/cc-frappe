a4e4f636d7bb84aa276757c880825738da41ad58
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2025-02-14 11:19:54 +0100

    testing

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,6 @@
-FROM frappe/erpnext:v15
+#FROM frappe/erpnext:v15
+FROM frappe/bench:latest
+
+COPY ./init.sh .
+
+CMD ["sh", "./init.sh"]
---
+++ init.sh
@@ -0,0 +1,43 @@
+#!bin/bash
+
+if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
+    echo "Bench already exists, skipping init"
+    cd frappe-bench
+    bench start
+else
+    echo "Creating new bench..."
+fi
+
+export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
+echo $PATH
+
+bench init --skip-redis-config-generation frappe-bench
+
+cd frappe-bench
+
+# Use containers instead of localhost
+bench set-mariadb-host mysql-capsule-kapw-mysql-master:3306
+bench set-redis-cache-host redis-capsule-yndv.erp-next-staging-eiho:6379
+bench set-redis-queue-host redis-capsule-yndv.erp-next-staging-eiho:6379
+bench set-redis-socketio-host redis-capsule-yndv.erp-next-staging-eiho:6379
+
+# Remove redis, watch from Procfile
+sed -i '/redis/d' ./Procfile
+sed -i '/watch/d' ./Procfile
+
+bench get-app erpnext
+bench get-app hrms
+
+bench new-site erp-next-qnxo.codecapsules.co.za \
+--force \
+--mariadb-root-password 1cc5083f-c881-87b3-c065-54956ed62c73 \
+--admin-password admin \
+--no-mariadb-socket
+
+bench --site erp-next-qnxo.codecapsules.co.za install-app hrms
+bench --site erp-next-qnxo.codecapsules.co.za set-config developer_mode 1
+bench --site erp-next-qnxo.codecapsules.co.za enable-scheduler
+bench --site erp-next-qnxo.codecapsules.co.za clear-cache
+bench use erp-next-qnxo.codecapsules.co.za
+
+bench start
